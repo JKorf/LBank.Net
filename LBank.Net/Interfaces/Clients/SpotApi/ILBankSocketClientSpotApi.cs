2ecd551a2d629c8145f36cbb26da9962180cf05c
Jkorf <jankorf91@gmail.com>
2026-08-05 15:01:26 +0200

    Updated comments

--- LBank.Net/Interfaces/Clients/SpotApi/ILBankSocketClientSpotApi.cs
+++ LBank.Net/Interfaces/Clients/SpotApi/ILBankSocketClientSpotApi.cs
@@ -15,31 +15,65 @@ namespace LBank.Net.Interfaces.Clients.SpotApi
     public interface ILBankSocketClientSpotApi : ISocketApiClient<LBankCredentials>, IDisposable
     {
         /// <summary>
-        /// 
-        /// <para><a href="XXX" /></para>
+        /// Subscribe to trade updates for a symbol
+        /// <para><a href="https://www.lbank.com/docs/#trade-record" /></para>
         /// </summary>
-        /// <param name="onMessage">The event handler for the received data</param>
+        /// <param name="symbol">The symbols to subscribe to, for example `eth_usdt`</param>
+        /// <param name="onMessage">The data handler</param>
         /// <param name="ct">Cancellation token for closing this subscription</param>
-        /// <returns>A stream subscription. This stream subscription can be used to be notified when the socket is disconnected/reconnected</returns>
+        /// <returns>A stream subscription. This stream subscription can be used to be notified when the socket is disconnected/reconnected and to unsubscribe</returns>
         Task<WebSocketResult<UpdateSubscription>> SubscribeToTradeUpdatesAsync(string symbol, Action<DataEvent<LBankTradeUpdate>> onMessage, CancellationToken ct = default);
 
         /// <summary>
-        /// 
+        /// Subscribe to kline updates for a symbol
+        /// <para><a href="https://www.lbank.com/docs/#subscription-of-k-line-data" /></para>
         /// </summary>
-        /// <param name="symbol"></param>
-        /// <param name="interval"></param>
-        /// <param name="onMessage"></param>
-        /// <param name="ct"></param>
-        /// <returns></returns>
+        /// <param name="symbol">The symbols to subscribe to, for example `eth_usdt`</param>
+        /// <param name="interval">The kline interval</param>
+        /// <param name="onMessage">The data handler</param>
+        /// <param name="ct">Cancellation token for closing this subscription</param>
+        /// <returns>A stream subscription. This stream subscription can be used to be notified when the socket is disconnected/reconnected and to unsubscribe</returns>
         Task<WebSocketResult<UpdateSubscription>> SubscribeToKlineUpdatesAsync(string symbol, StreamKlineInterval interval, Action<DataEvent<LBankKlineUpdate>> onMessage, CancellationToken ct = default);
 
-
+        /// <summary>
+        /// Subscribe to snapshot updates for an order book
+        /// <para><a href="https://www.lbank.com/docs/#market-depth" /></para>
+        /// </summary>
+        /// <param name="symbol">The symbols to subscribe to, for example `eth_usdt`</param>
+        /// <param name="depth">Book depth, 10, 50 or 100</param>
+        /// <param name="onMessage">The data handler</param>
+        /// <param name="ct">Cancellation token for closing this subscription</param>
+        /// <returns>A stream subscription. This stream subscription can be used to be notified when the socket is disconnected/reconnected and to unsubscribe</returns>
         Task<WebSocketResult<UpdateSubscription>> SubscribeToOrderBookUpdatesAsync(string symbol, int depth, Action<DataEvent<LBankOrderBookUpdate>> onMessage, CancellationToken ct = default);
 
+        /// <summary>
+        /// Subscribe to ticker updates for a symbol
+        /// <para><a href="https://www.lbank.com/docs/#market" /></para>
+        /// </summary>
+        /// <param name="symbol">The symbols to subscribe to, for example `eth_usdt`</param>
+        /// <param name="onMessage">The data handler</param>
+        /// <param name="ct">Cancellation token for closing this subscription</param>
+        /// <returns>A stream subscription. This stream subscription can be used to be notified when the socket is disconnected/reconnected and to unsubscribe</returns>
         Task<WebSocketResult<UpdateSubscription>> SubscribeToTickerUpdatesAsync(string symbol, Action<DataEvent<LBankTickerUpdate>> onMessage, CancellationToken ct = default);
 
+        /// <summary>
+        /// Subscribe to user order updates
+        /// <para><a href="https://www.lbank.com/docs/#update-subscribed-orders" /></para>
+        /// </summary>
+        /// <param name="listenKey">The listen key for the user account. If not provided listen key management will be handled internally</param>
+        /// <param name="onMessage">The data handler</param>
+        /// <param name="ct">Cancellation token for closing this subscription</param>
+        /// <returns>A stream subscription. This stream subscription can be used to be notified when the socket is disconnected/reconnected and to unsubscribe</returns>
         Task<WebSocketResult<UpdateSubscription>> SubscribeToOrderUpdatesAsync(string? listenKey, Action<DataEvent<LBankOrderUpdate>> onMessage, CancellationToken ct = default);
 
+        /// <summary>
+        /// Subscribe to user balance updates
+        /// <para><a href="https://www.lbank.com/docs/#update-subscribed-asset" /></para>
+        /// </summary>
+        /// <param name="listenKey">The listen key for the user account. If not provided listen key management will be handled internally</param>
+        /// <param name="onMessage">The data handler</param>
+        /// <param name="ct">Cancellation token for closing this subscription</param>
+        /// <returns>A stream subscription. This stream subscription can be used to be notified when the socket is disconnected/reconnected and to unsubscribe</returns>
         Task<WebSocketResult<UpdateSubscription>> SubscribeToBalanceUpdatesAsync(string? listenKey, Action<DataEvent<LBankBalanceUpdate>> onMessage, CancellationToken ct = default);
 
         /// <summary>
